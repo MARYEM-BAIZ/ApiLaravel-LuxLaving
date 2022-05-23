2f34dd772a8f83f925dfbd16994e0020dd009da3
MARYEM-BAIZ <baiz.maryem2018@gmail.com>
2022-05-23 16:28:38 +0200

    commit 5

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Prestation;
+use App\Models\ArticleCategorie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
     use HasFactory;
+
+    // setting models relations
+    public function articleCategorie(){
+
+        return $this->belongsTo(ArticleCategorie::class, 'article_cat', 'id');
+
+    }
+
+    public function prestation(){
+
+        return $this->belongsTo(Prestation::class, 'prestation_id', 'id');
+
+    }
 }
--- app/Models/Prestation.php
+++ app/Models/Prestation.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Article;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Prestation extends Model
 {
     use HasFactory;
+
+    // setting models relations
+    public function articles()
+    {
+
+        return $this->hasMany(Article::class);
+
+    }
 }
