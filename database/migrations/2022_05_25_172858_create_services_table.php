ada97012aeb71d52eaa3ec7647425e6450ce640f
MARYEM-BAIZ <baiz.maryem2018@gmail.com>
2022-05-25 19:31:34 +0200

    commit 7

--- database/migrations/2022_05_25_172858_create_services_table.php
+++ database/migrations/2022_05_25_172858_create_services_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('prestations', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('prestations');
+        Schema::dropIfExists('services');
     }
 };
